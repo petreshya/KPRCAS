e396ddee1fee9861b1ccf80de5519e4fcbd9c9ef
Petreshya <petreshya.p@gmail.com>
2025-06-06 15:49:04 +0530

    change in index

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log(5==='5');
+let i=0
+do{
+    i++;
+    console.log(i);
+}while(i<5)
+    function greet(name){
+        return'hello'+name
+    }
+        console.log(greet('ronaldo'));    
+    
+        function greet(name,age){
+            return `hello ${name} is ${age} old`
+        }
+        console.log(greet('Ronaldo',40));
+        
